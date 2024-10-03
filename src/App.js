f5ee3967b4c06c624db4978504ea57facd03ab23
aradvazir <aradvazir@gmail.com>
2024-10-03 14:55:15 +0330

    conflict bug fixed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Form from "./Form";
-import DataTable from "./DataTable";
-import Login from "./Login";
-import Menu from "./Menu"
+// import DataTable from "./DataTable";
+// import Login from "./Login";
+// import Menu from "./Menu";
 import "./App.css";
 
 function App() {
