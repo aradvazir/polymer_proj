c3c713cadd0033eb78b0e84787926f87d34ba1ec
aradvazir <aradvazir@gmail.com>
2024-10-03 15:15:20 +0330

    Menu updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 // import Form from "./Form";
 // import DataTable from "./DataTable";
-import Login from "./Login";
-// import Menu from "./Menu";
+// import Login from "./Login";
+import Menu from "./Menu";
 import "./App.css";
 
 function App() {
   return (
-    <Login/>
+    <Menu/>
   );
 }
 
