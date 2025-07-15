4eb47aa4cc60e2d75d3017a469174f6aa1eb96b0
amirabbas <amirsoltani1381@gmail.com>
2025-07-15 10:09:20 +0330

    change url

--- src/consts.js
+++ src/consts.js
@@ -1,7 +1,7 @@
 // export const baseUrl = "/";
-export const baseUrl = "http://localhost:8000/api/";
+//export const baseUrl = "http://localhost:8000/api/";
 // export const baseUrl = "https://37.255.233.131:7007/api/";
-//export const baseUrl = "https://172.16.87.18/api/"
+export const baseUrl = "https://172.16.87.18/api/"
 
 export const translations = {
   allproducts: "همه محصولات",
